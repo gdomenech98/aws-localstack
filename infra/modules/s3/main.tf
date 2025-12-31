f60322312676fecde4b3608d5c80522438d516fc
Gerard <gerard@protofy.xyz>
2025-12-31 17:44:46 +0100

    adds missing s3 module

---
+++ infra/modules/s3/main.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+
+  force_destroy = false
+}
