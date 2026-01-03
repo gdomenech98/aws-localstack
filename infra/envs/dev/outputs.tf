758cf62c3a8ed3b4a3f8ca257c49674785c48c9d
Gerard <gerard@protofy.xyz>
2026-01-03 17:24:42 +0100

    adds outputs

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -1,3 +1,15 @@
 output "bucket_id" {
   value = module.s3.bucket_id
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
